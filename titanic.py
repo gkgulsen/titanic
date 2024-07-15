632f0e3f0d7dfdba6ede8970fcf906d04cba7b05
gkadi <gkadi@GGULSEN>
2024-07-15 23:10:03 +0300

    second commit

--- titanic.py
+++ titanic.py
@@ -1,10 +1,10 @@
-import numpy as np # linear algebra
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import numpy as np # type: ignore # linear algebra
+import pandas as pd # type: ignore # data processing, CSV file I/O (e.g. pd.read_csv)
 
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt # type: ignore
 plt.style.use("seaborn-v0_8-whitegrid")
 
-import seaborn as sns
+import seaborn as sns # type: ignore
 
 from collections import Counter
 
@@ -20,4 +20,93 @@ test_PassengerId = test_df["PassengerId"]
 #train_df.head()
 
 #train_df.describe() #nümerik kolonlarla ilgili bilgi verir
-print(train_df.describe()   )
+
+def bar_plot(variable):
+    """
+        input: variable ex: "Sex"
+        output: bar plot & value count
+    """
+    # get feature
+    var = train_df[variable]
+    # count number of categorical variable(value/sample)
+    varValue = var.value_counts()
+    
+    # visualize
+    plt.figure(figsize = (9,3))
+    plt.bar(varValue.index, varValue)
+    plt.xticks(varValue.index, varValue.index.values)
+    plt.ylabel("Frequency")
+    plt.title(variable)
+    plt.show()
+    print("{}: \n {}".format(variable,varValue))
+    
+    
+category1 = ["Survived","Sex","Pclass","Embarked","SibSp", "Parch"]
+for c in category1:
+    bar_plot(c)
+    
+    
+category2 = ["Cabin", "Name", "Ticket"]
+for c in category2:
+    print("{} \n".format(train_df[c].value_counts()))   
+    
+def plot_hist(variable):
+    plt.figure(figsize = (9,3))
+    plt.hist(train_df[variable], bins = 50)
+    plt.xlabel(variable)
+    plt.ylabel("Frequency")
+    plt.title("{} distribution with hist".format(variable))
+    plt.show()
+    
+numericVar = ["Fare", "Age","PassengerId"]
+for n in numericVar:
+    plot_hist(n)
+    
+#%% Plcass vs Survived
+
+print(train_df[["Pclass","Survived"]].groupby(["Pclass"], as_index = False).mean().sort_values(by="Survived",ascending = False))
+  
+#%%  Sibsp vs Survived
+train_df[["SibSp","Survived"]].groupby(["SibSp"], as_index = False).mean().sort_values(by="Survived",ascending = False)  
+    
+#%% Parch vs Survived
+
+train_df[["Parch","Survived"]].groupby(["Parch"], as_index = False).mean().sort_values(by="Survived",ascending = False)  
+
+#%% Outlier Detection
+def detect_outliers(df,features):
+    outlier_indices = []
+    
+    for c in features:
+        # 1st quartile
+        Q1 = np.percentile(df[c],25)
+        # 3rd quartile
+        Q3 = np.percentile(df[c],75)
+        # IQR
+        IQR = Q3 - Q1
+        # Outlier step
+        outlier_step = IQR * 1.5
+        # detect outlier and their indeces
+        outlier_list_col = df[(df[c] < Q1 - outlier_step) | (df[c] > Q3 + outlier_step)].index
+        # store indeces
+        outlier_indices.extend(outlier_list_col)
+        
+    
+ 
+    outlier_indices = Counter(outlier_indices)  
+    multiple_outliers = list(i for i, v in outlier_indices.items() if v > 2)
+    
+    return multiple_outliers
+#%% 
+ 
+
+
+#%% 
+
+train_df.loc[detect_outliers(train_df,["Age","SibSp","Parch","Fare"])]  
+    
+    
+    
+    
+    
+
